aca6988566213f128d2b4d040bcdf1c786a228b1
cxmarcelo <marcelo.c.b22@gmail.com>
2022-09-03 16:30:14 -0300

    Config basicas

--- greeting-service/src/main/java/br/com/marcelo/greetingservice/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/com/marcelo/greetingservice/controllers/GreetingController.java
@@ -2,10 +2,12 @@ package br.com.marcelo.greetingservice.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marcelo.greetingservice.config.GreetingConfig;
 import br.com.marcelo.greetingservice.model.Greeting;
 
 @RestController
@@ -13,8 +15,18 @@ public class GreetingController {
 
 	private final AtomicLong counter = new AtomicLong();
 	
+	private static final String TEMPLATE = "%s, %s!";
+	
+	@Autowired
+	private GreetingConfig config;
+	
 	@RequestMapping("greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-		return new Greeting(counter.incrementAndGet(), name);
+		
+		if(name.isEmpty()) {
+			name = config.getDefaultValue();
+		}
+		
+		return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, config.getGreeting(), name));
 	}
 }
